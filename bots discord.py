14e11fa9174823063f0c399654863084d73149b5
the-special-one <103434433+the-special-one@users.noreply.github.com>
2022-10-02 23:02:42 +0200

    Update bots discord.py

--- bots discord.py
+++ bots discord.py
@@ -22,19 +22,12 @@ async def on_message(message):
         messages = await message.channel.history(limit=number + 1).flatten()
         for each_message in messages:
             await each_message.delete()
-"""
-@client.event
-async def on_message(message):
-
-	if message.content.startswith("!hot"):
-		nombre = int(message.content.split()[1])
-		await message.channel.send(f"{random.randint(1,nombre)}")"""
 
 @client.event
 async def on_member_join(member):
-	commandes_channel: discord.TextChannel = client.get_channel(932009769844244510)
+	commandes_channel: discord.TextChannel = client.get_channel("channel ID)
 	await commandes_channel.send(content=f"Bienvenida sur le serveur {member.display_name} !") 
 
-client.run("OTI1NTQ1NTQ4OTEwNTYzNDIw.Ycurdg.0TX3NA5O1kTavxGRNA3j7Zu16Cw")
+client.run("MY TOKEN")
 
 
